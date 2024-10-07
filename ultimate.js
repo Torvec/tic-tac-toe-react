79d5fca1549fa5a4f7d8c5c8d80e05f2eae584ed
Edward Vonschondorf <edvonschondorf@gmail.com>
2024-10-07 16:42:40 -0700

    Refactored state entirely, refactored player setting, refactored UI related functions for cells and gameboard, and many other changes throughout

--- ultimate.js
+++ ultimate.js
@@ -1,8 +1,38 @@
-//* Initially the first playable grid is the center grid
-//* When a player makes their move in a grid, the next playable grid is determined by the index of the cell which is then passed to the setActiveGrid method
-// TODO: However that grid is only playable if it is not won or drawn, if the grid is unplayable then the player can play anywhere that has empty cells
-// TODO: Also need to make sure the previous grid's state is changed to unplayable but only if the next grid is playable
-// TODO: Also need to be able to set EVERY available grid to playable if the player can play anywhere
+const STATES = {
+  PLAYER: {
+    X: "X",
+    O: "O",
+  },
+  CELL: {
+    E: " ",
+    X: "X",
+    O: "O",
+  },
+  GRID: {
+    ACTIVE: "active",
+    INACTIVE: "inactive",
+    X: "X",
+    O: "O",
+    DRAW: "draw",
+  },
+  BOARD: {
+    PLAY: "play",
+    X: "X",
+    O: "O",
+    DRAW: "draw",
+  },
+};
+
+const WINNING_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 class InputHandler {
   constructor(game) {
@@ -37,18 +67,10 @@ class GameCell {
     this.y = y;
     this.width = width;
     this.height = height;
-    this.cellStates = {
-      EMPTY: " ",
-      FILLED_X: "X",
-      FILLED_O: "O",
-      DISABLED_X: "disabled_X",
-      DISABLED_O: "disabled_O",
-      DISABLED_DRAW: "disabled_draw",
-    };
     this.init();
   }
   init() {
-    this.currentState = this.cellStates.EMPTY;
+    this.currentState = STATES.CELL.E;
   }
   isPointerOver(pointer) {
     return (
@@ -59,21 +81,27 @@ class GameCell {
     );
   }
   setGameCellState(newState) {
-    if (this.currentState === this.cellStates.EMPTY)
-      this.currentState = newState;
+    if (this.currentState === STATES.CELL.E) this.currentState = newState;
   }
-  update() {}
-  draw() {
-    // This determines the color of the lines for each box
+  drawCellBorder() {
     c.save();
     c.strokeStyle = "black";
-    c.lineWidth = 1;
+    c.lineWidth = 2;
     c.strokeRect(this.x, this.y, this.width, this.height);
-    c.fillStyle = "grey";
+    c.restore();
+  }
+  drawCellBg() {
+    c.save();
+    if (this.currentState === STATES.CELL.E) c.fillStyle = "lightgray";
+    if (this.currentState === STATES.CELL.X) c.fillStyle = "darkblue";
+    if (this.currentState === STATES.CELL.O) c.fillStyle = "maroon";
     c.fillRect(this.x, this.y, this.width, this.height);
     c.restore();
+  }
+  drawCellContent() {
     c.save();
-    c.fillStyle = "black";
+    if (this.currentState === STATES.CELL.X) c.fillStyle = "lightblue";
+    if (this.currentState === STATES.CELL.O) c.fillStyle = "pink";
     c.font = "bold 64px Monospace";
     c.textAlign = "center";
     c.textBaseline = "middle";
@@ -84,6 +112,11 @@ class GameCell {
     );
     c.restore();
   }
+  render() {
+    this.drawCellBorder();
+    this.drawCellBg();
+    this.drawCellContent();
+  }
 }
 
 class GameGrid {
@@ -92,17 +125,10 @@ class GameGrid {
     this.y = y;
     this.width = width;
     this.height = height;
-    this.gridStates = {
-      ACTIVE: "active",
-      INACTIVE: "inactive",
-      X_WON: "x_won",
-      O_WON: "o_won",
-      DRAW: "draw",
-    };
     this.init();
   }
   init() {
-    this.currentState = this.gridStates.INACTIVE;
+    this.currentState = STATES.GRID.INACTIVE;
     this.cells = [];
     this.setupGameGrid();
   }
@@ -126,29 +152,24 @@ class GameGrid {
   }
   isPlayable() {
     return (
-      this.currentState !== this.gridStates.X_WON &&
-      this.currentState !== this.gridStates.O_WON &&
-      this.currentState !== this.gridStates.DRAW
+      this.currentState === STATES.GRID.ACTIVE ||
+      this.currentState === STATES.GRID.INACTIVE
+    );
+  }
+  isNotPlayable() {
+    return (
+      this.currentState === STATES.GRID.X ||
+      this.currentState === STATES.GRID.O ||
+      this.currentState === STATES.GRID.DRAW
     );
   }
   isGridWon() {
-    const winningCombinations = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    const cellStates = this.cells[0].cellStates;
-    for (let combination of winningCombinations) {
+    for (let combination of WINNING_COMBOS) {
       const [a, b, c] = combination;
       const cellA = this.cells[a].currentState;
       const cellB = this.cells[b].currentState;
       const cellC = this.cells[c].currentState;
-      if (cellA === cellB && cellB === cellC && cellA !== cellStates.EMPTY) {
+      if (cellA === cellB && cellB === cellC && cellA !== STATES.CELL.E) {
         return true;
       }
     }
@@ -156,53 +177,44 @@ class GameGrid {
   }
   isGridDraw() {
     for (let cell of this.cells) {
-      if (cell.currentState === cell.cellStates.EMPTY) {
+      if (cell.currentState === STATES.CELL.E) {
         return false;
       }
     }
     return true;
   }
-  update() {
-    this.cells.forEach((cell) => cell.update());
-    if (this.isGridWon()) {
-      this.currentState =
-        this.cells[0].currentState === this.cells[0].cellStates.FILLED_X
-          ? this.gridStates.X_WON
-          : this.gridStates.O_WON;
-    }
-    if (this.isGridDraw()) {
-      this.currentState = this.gridStates.DRAW;
-    }
-  }
-  draw() {
-    this.cells.forEach((cell) => cell.draw());
+  drawGridState() {
     switch (this.currentState) {
-      case this.gridStates.ACTIVE:
+      case STATES.GRID.ACTIVE:
         c.save();
-        c.strokeStyle = "rgba(0, 144, 0, 1)";
+        c.strokeStyle = "rgba(0, 196, 0, 1)";
         c.lineWidth = 6;
         c.strokeRect(this.x, this.y, this.width, this.height);
         c.restore();
         break;
-      case this.gridStates.X_WON:
+      case STATES.GRID.X:
         c.save();
         c.fillStyle = "rgba(0, 0, 144, 0.25)";
         c.fillRect(this.x, this.y, this.width, this.height);
         c.restore();
         break;
-      case this.gridStates.O_WON:
+      case STATES.GRID.O:
         c.save();
         c.fillStyle = "rgba(144, 0, 0, 0.25)";
         c.fillRect(this.x, this.y, this.width, this.height);
         c.restore();
         break;
-      case this.gridStates.DRAW:
+      case STATES.GRID.DRAW:
         c.save();
         c.fillStyle = "rgba(64, 64, 64, 0.25)";
         c.fillRect(this.x, this.y, this.width, this.height);
         c.restore();
         break;
     }
+  }
+  render() {
+    this.cells.forEach((cell) => cell.render());
+    this.drawGridState();
   }
 }
 
@@ -214,20 +226,14 @@ class GameBoard {
     this.y = 32;
     this.width = this.game.width - 32;
     this.height = this.game.height - 64;
-    this.player = { x: "X", o: "O" };
-    this.gameBoardStates = {
-      PLAY: "play",
-      X_WON: "x_won",
-      O_WON: "o_won",
-      DRAW: "draw",
-    };
     this.init();
   }
   init() {
-    this.currentPlayer = Math.random() <= 0.5 ? this.player.x : this.player.o;
+    this.currentPlayer = null;
+    this.setCurrentPlayer();
     this.gameGrids = [];
     this.setupGameBoard();
-    this.setGameBoardState(this.gameBoardStates.PLAY);
+    this.setGameBoardState(STATES.BOARD.PLAY);
     this.setActiveGrid(null, 4);
   }
   setupGameBoard() {
@@ -249,93 +255,76 @@ class GameBoard {
     this.currentState = newState;
   }
   setActiveGrid(prevIndex, newIndex) {
-    this.gameGrids[newIndex].setGameGridState(
-      this.gameGrids[newIndex].gridStates.ACTIVE
-    );
+    const nextGrid = this.gameGrids[newIndex];
+    const prevGrid = this.gameGrids[prevIndex];
+    if (nextGrid.isNotPlayable()) {
+      this.gameGrids.forEach((grid) => {
+        if (grid.currentState === STATES.GRID.INACTIVE) {
+          grid.setGameGridState(STATES.GRID.ACTIVE);
+        }
+      });
+    } else if (nextGrid.isPlayable()) {
+      nextGrid.setGameGridState(STATES.GRID.ACTIVE);
+    }
     if (prevIndex !== null && prevIndex !== newIndex) {
-      this.gameGrids[prevIndex].setGameGridState(
-        this.gameGrids[prevIndex].gridStates.INACTIVE
-      );
+      prevGrid.setGameGridState(STATES.GRID.INACTIVE);
     }
   }
   handleClick() {
     this.gameGrids.forEach((grid, gridIndex) => {
-      if (grid.currentState === grid.gridStates.ACTIVE) {
+      if (grid.currentState === STATES.GRID.ACTIVE) {
         grid.cells.forEach((cell, cellIndex) => {
           if (cell.isPointerOver(this.input.pointer)) {
-            cell.setGameCellState(
-              this.currentPlayer === this.player.x
-                ? cell.cellStates.FILLED_X
-                : cell.cellStates.FILLED_O
-            );
-            if (grid.isGridWon()) {
-              grid.setGameGridState(
-                this.currentPlayer === this.player.x
-                  ? grid.gridStates.X_WON
-                  : grid.gridStates.O_WON
-              );
-              console.log(this.currentPlayer + " has won the grid!");
-            }
-            if (this.isGameWon()) {
-              this.setGameBoardState(
-                this.currentPlayer === this.player.x
-                  ? this.gameBoardStates.X_WON
-                  : this.gameBoardStates.O_WON
-              );
-              console.log(this.currentPlayer + " has won the game!");
-              return;
-            }
-            this.changeCurrentPlayer();
+            cell.setGameCellState(STATES.CELL[this.currentPlayer]);
+            this.setCurrentPlayer();
             this.setActiveGrid(gridIndex, cellIndex);
           }
         });
       }
     });
   }
-  changeCurrentPlayer() {
-    if (this.currentPlayer === this.player.x)
-      this.currentPlayer = this.player.o;
-    else this.currentPlayer = this.player.x;
+  setCurrentPlayer() {
+    switch (this.currentPlayer) {
+      case null:
+        this.currentPlayer =
+          Math.random() <= 0.5 ? STATES.PLAYER.X : STATES.PLAYER.O;
+        break;
+      case STATES.PLAYER.X:
+        this.currentPlayer = STATES.PLAYER.O;
+        break;
+      case STATES.PLAYER.O:
+        this.currentPlayer = STATES.PLAYER.X;
+        break;
+    }
   }
   isGameWon() {
-    const winningCombinations = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    const gridStates = this.gameGrids[0].gridStates;
-    for (let combination of winningCombinations) {
+    for (let combination of WINNING_COMBOS) {
       const [a, b, c] = combination;
       const gridA = this.gameGrids[a].currentState;
       const gridB = this.gameGrids[b].currentState;
       const gridC = this.gameGrids[c].currentState;
-      // Active and Inactive states mean the grid is playable so it is equivalent to the EMPTY state in a cell
       if (
         gridA === gridB &&
         gridB === gridC &&
-        gridA !== gridStates.INACTIVE &&
-        gridA !== gridStates.ACTIVE
+        gridA !== STATES.GRID.INACTIVE &&
+        gridA !== STATES.GRID.ACTIVE
       ) {
         return true;
       }
     }
+    return false;
   }
   isGameDraw() {
     for (let grid of this.gameGrids) {
       for (let cell of grid.cells) {
-        if (cell.currentState === cell.cellStates.EMPTY) {
+        if (cell.currentState === STATES.CELL.E) {
           return false;
         }
       }
     }
     return true;
   }
-  currentPlayerIndicator() {
+  displayCurrentPlayer() {
     c.save();
     c.fillStyle = "black";
     c.font = "bold 32px Monospace";
@@ -348,22 +337,32 @@ class GameBoard {
     );
     c.restore();
   }
-  update() {
-    this.gameGrids.forEach((grid) => grid.update());
+  displayWinningPlayer() {
+    c.save();
+    c.fillStyle = "black";
+    c.font = "bold 32px Monospace";
+    c.textAlign = "center";
+    c.textBaseline = "middle";
+    c.fillText(
+      this.currentPlayer + " has won!",
+      this.width * 0.5,
+      this.height - 32
+    );
   }
-  draw() {
-    this.currentPlayerIndicator();
-    this.gameGrids.forEach((grid) => grid.draw());
-    if (this.isGameWon()) {
-      c.fillText(
-        this.currentPlayer + " has won!",
-        this.width * 0.5,
-        this.height - 32
-      );
-    }
-    if (this.isGameDraw()) {
-      c.fillText("It's a draw!", this.width * 0.5, this.height - 32);
-    }
+  displayDrawMessage() {
+    c.save();
+    c.fillStyle = "black";
+    c.font = "bold 32px Monospace";
+    c.textAlign = "center";
+    c.textBaseline = "middle";
+    c.fillText("It's a draw!", this.width * 0.5, this.height - 32);
+    c.restore();
+  }
+  render() {
+    this.displayCurrentPlayer();
+    this.gameGrids.forEach((grid) => grid.render());
+    if (this.isGameWon()) this.displayWinningPlayer();
+    if (this.isGameDraw()) this.displayDrawMessage();
   }
 }
 
@@ -375,8 +374,7 @@ class Game {
     this.gameBoard = new GameBoard(this);
   }
   render() {
-    this.gameBoard.update();
-    this.gameBoard.draw();
+    this.gameBoard.render();
   }
 }
 
