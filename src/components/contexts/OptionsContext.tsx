bd6714b68d59bf4dfac9987b336f4bd9068098a4
Edward Vonschondorf <edvonschondorf@gmail.com>
2024-10-19 15:48:30 -0700

    Removed more code related to selecting different opponents

--- src/components/contexts/OptionsContext.tsx
+++ src/components/contexts/OptionsContext.tsx
@@ -8,13 +8,9 @@ import {
 
 export type Mode = "classic" | "ultimate" | undefined;
 
-export type Opponent = "pvp" | "pvc" | undefined;
-
 type OptionsContextType = {
   gameMode: Mode;
   setGameMode: Dispatch<SetStateAction<Mode>>;
-  opponent: Opponent;
-  setOpponent: Dispatch<SetStateAction<Opponent>>;
 };
 
 type OptionsProviderProps = {
@@ -27,15 +23,12 @@ export const OptionsContext = createContext<OptionsContextType | undefined>(
 
 export const OptionsProvider = ({ children }: OptionsProviderProps) => {
   const [gameMode, setGameMode] = useState<Mode>(undefined);
-  const [opponent, setOpponent] = useState<Opponent>(undefined);
 
   return (
     <OptionsContext.Provider
       value={{
         gameMode,
         setGameMode,
-        opponent,
-        setOpponent,
       }}
     >
       {children}
